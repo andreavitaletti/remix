7688a2923eec5c1d151cec37db9070ace9693982
andreavitaletti <vitaletti@dis.uniroma1.it>
2025-07-22 19:11:35 +0200

    primo commit

--- contracts/1_Storage.sol
+++ contracts/1_Storage.sol
@@ -7,6 +7,9 @@ pragma solidity >=0.8.2 <0.9.0;
  * @dev Store & retrieve value in a variable
  * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
  */
+
+ // test 
+ 
 contract Storage {
 
     uint256 number;
